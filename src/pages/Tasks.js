99d9184620d277ddaf2004ff520f80cb3457eba9
Mateus Sousa <sousa.programador@gmail.com>
2024-08-27 13:20:59 -0300

    styles modal users

--- src/pages/Tasks.js
+++ src/pages/Tasks.js
@@ -2,109 +2,143 @@ import React, { useState } from 'react';
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 import TaskModal from '../components/TaskModal';
 
-const initialTasks = {
-  pending: [
-    { id: '1', content: 'Tarefa 1' },
-    { id: '2', content: 'Tarefa 2' },
+const tarefasIniciais = {
+  pendente: [
+    {
+      id: '1',
+      title: 'Tarefa 1',
+      description: 'Descrição da Tarefa 1',
+      image: 'https://via.placeholder.com/150',
+      userId: '123',
+      status: 'pendente',
+    },
+    {
+      id: '2',
+      title: 'Tarefa 2',
+      description: 'Descrição da Tarefa 2',
+      image: 'https://via.placeholder.com/150',
+      userId: '124',
+      status: 'pendente',
+    },
   ],
-  inProgress: [{ id: '3', content: 'Tarefa 3' }],
-  completed: [{ id: '4', content: 'Tarefa 4' }],
-  cancelled: [],
+  emProgresso: [
+    {
+      id: '3',
+      title: 'Tarefa 3',
+      description: 'Descrição da Tarefa 3',
+      image: 'https://via.placeholder.com/150',
+      userId: '125',
+      status: 'emProgresso',
+    },
+  ],
+  concluido: [
+    {
+      id: '4',
+      title: 'Tarefa 4',
+      description: 'Descrição da Tarefa 4',
+      image: 'https://via.placeholder.com/150',
+      userId: '126',
+      status: 'concluido',
+    },
+  ],
+  cancelado: [],
 };
 
 function Tasks() {
-  const [tasks, setTasks] = useState(initialTasks);
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [editingTask, setEditingTask] = useState(null);
-  const [currentColumn, setCurrentColumn] = useState(null);
+  const [tarefas, setTarefas] = useState(tarefasIniciais);
+  const [modalAberto, setModalAberto] = useState(false);
+  const [tarefaEditando, setTarefaEditando] = useState(null);
+  const [colunaAtual, setColunaAtual] = useState(null);
 
   const handleDragEnd = (result) => {
     const { source, destination } = result;
     if (!destination) return;
 
-    const sourceCol = source.droppableId;
-    const destCol = destination.droppableId;
-    const sourceTasks = Array.from(tasks[sourceCol]);
-    const destTasks = Array.from(tasks[destCol]);
-    const [movedTask] = sourceTasks.splice(source.index, 1);
+    const colunaOrigem = source.droppableId;
+    const colunaDestino = destination.droppableId;
+    const tarefasOrigem = Array.from(tarefas[colunaOrigem]);
+    const tarefasDestino = Array.from(tarefas[colunaDestino]);
+    const [tarefaMovida] = tarefasOrigem.splice(source.index, 1);
 
-    destTasks.splice(destination.index, 0, movedTask);
+    tarefaMovida.status = colunaDestino; // Atualiza o status da tarefa
 
-    setTasks({
-      ...tasks,
-      [sourceCol]: sourceTasks,
-      [destCol]: destTasks,
+    tarefasDestino.splice(destination.index, 0, tarefaMovida);
+
+    setTarefas({
+      ...tarefas,
+      [colunaOrigem]: tarefasOrigem,
+      [colunaDestino]: tarefasDestino,
     });
   };
 
-  const handleOpenModal = (task = null, columnId = null) => {
-    if (task) {
-      setEditingTask(task);
+  const handleAbrirModal = (tarefa = null, colunaId = null) => {
+    if (tarefa) {
+      setTarefaEditando(tarefa);
     } else {
-      setEditingTask(null);
+      setTarefaEditando(null);
     }
-    setCurrentColumn(columnId);
-    setIsModalOpen(true);
+    setColunaAtual(colunaId);
+    setModalAberto(true);
   };
 
-  const handleCloseModal = () => {
-    setEditingTask(null);
-    setCurrentColumn(null);
-    setIsModalOpen(false);
+  const handleFecharModal = () => {
+    setTarefaEditando(null);
+    setColunaAtual(null);
+    setModalAberto(false);
   };
 
-  const handleSave = (newTaskContent) => {
-    if (editingTask) {
-      const updatedTasks = {
-        ...tasks,
-        [currentColumn]: tasks[currentColumn].map((task) =>
-          task.id === editingTask.id
-            ? { ...task, content: newTaskContent }
-            : task
+  const handleSalvar = (novaTarefa) => {
+    if (tarefaEditando) {
+      const tarefasAtualizadas = {
+        ...tarefas,
+        [colunaAtual]: tarefas[colunaAtual].map((tarefa) =>
+          tarefa.id === tarefaEditando.id
+            ? { ...novaTarefa, status: colunaAtual }
+            : tarefa
         ),
       };
-      setTasks(updatedTasks);
-    } else if (currentColumn) {
-      const updatedTasks = {
-        ...tasks,
-        [currentColumn]: [
-          ...tasks[currentColumn],
-          { id: `${Date.now()}`, content: newTaskContent },
+      setTarefas(tarefasAtualizadas);
+    } else if (colunaAtual) {
+      const tarefasAtualizadas = {
+        ...tarefas,
+        [colunaAtual]: [
+          ...tarefas[colunaAtual],
+          { ...novaTarefa, id: `${Date.now()}`, status: colunaAtual },
         ],
       };
-      setTasks(updatedTasks);
+      setTarefas(tarefasAtualizadas);
     }
-    handleCloseModal();
+    handleFecharModal();
   };
 
-  const handleDelete = (colId, taskId) => {
-    const updatedTasks = {
-      ...tasks,
-      [colId]: tasks[colId].filter((task) => task.id !== taskId),
+  const handleDeletar = (colunaId, tarefaId) => {
+    const tarefasAtualizadas = {
+      ...tarefas,
+      [colunaId]: tarefas[colunaId].filter((tarefa) => tarefa.id !== tarefaId),
     };
-    setTasks(updatedTasks);
+    setTarefas(tarefasAtualizadas);
   };
 
   return (
     <div className='flex flex-col space-y-4'>
       <DragDropContext onDragEnd={handleDragEnd}>
         <div className='flex space-x-4'>
-          {Object.keys(tasks).map((colId) => (
-            <Droppable key={colId} droppableId={colId}>
+          {Object.keys(tarefas).map((colunaId) => (
+            <Droppable key={colunaId} droppableId={colunaId}>
               {(provided) => (
                 <div
                   ref={provided.innerRef}
                   {...provided.droppableProps}
                   className='bg-gray-100 p-4 rounded w-1/4 cursor-pointer'
-                  onClick={() => handleOpenModal(null, colId)}
+                  onClick={() => handleAbrirModal(null, colunaId)}
                 >
                   <h2 className='text-lg font-semibold mb-2 capitalize'>
-                    {colId}
+                    {colunaId}
                   </h2>
-                  {tasks[colId].map((task, index) => (
+                  {tarefas[colunaId].map((tarefa, index) => (
                     <Draggable
-                      key={task.id}
-                      draggableId={task.id}
+                      key={tarefa.id}
+                      draggableId={tarefa.id}
                       index={index}
                     >
                       {(provided) => (
@@ -114,16 +148,29 @@ function Tasks() {
                           {...provided.dragHandleProps}
                           className='bg-white p-4 mb-2 rounded shadow-md'
                         >
-                          <p className='text-sm'>{task.content}</p>
+                          <h3 className='text-sm font-semibold'>
+                            {tarefa.title}
+                          </h3>
+                          <p className='text-sm'>{tarefa.description}</p>
+                          {tarefa.image && (
+                            <img
+                              src={tarefa.image}
+                              alt='Imagem da tarefa'
+                              className='w-full h-32 object-cover mt-2 rounded'
+                            />
+                          )}
+                          <div className='text-xs text-gray-500 mt-2'>
+                            User ID: {tarefa.userId}
+                          </div>
                           <div className='mt-2 flex justify-between'>
                             <button
-                              onClick={() => handleOpenModal(task, colId)}
+                              onClick={() => handleAbrirModal(tarefa, colunaId)}
                               className='text-blue-500'
                             >
                               Editar
                             </button>
                             <button
-                              onClick={() => handleDelete(colId, task.id)}
+                              onClick={() => handleDeletar(colunaId, tarefa.id)}
                               className='text-red-500'
                             >
                               Remover
@@ -142,10 +189,10 @@ function Tasks() {
       </DragDropContext>
 
       <TaskModal
-        isOpen={isModalOpen}
-        onRequestClose={handleCloseModal}
-        onSave={handleSave}
-        editingTask={editingTask}
+        isOpen={modalAberto}
+        onRequestClose={handleFecharModal}
+        onSave={handleSalvar}
+        editingTask={tarefaEditando}
       />
     </div>
   );
