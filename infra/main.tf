5561de3748f6ccfd2d9818cfdfea1f10cfe55c2c
Mateus Sousa <sousa.programador@gmail.com>
2024-08-27 22:38:23 -0300

    teste de pipeline

--- infra/main.tf
+++ infra/main.tf
@@ -32,11 +32,3 @@ resource "aws_s3_bucket_policy" "static_site_policy" {
 }
 POLICY
 }
-
-output "s3_bucket_name" {
-  value = aws_s3_bucket.static_site.bucket
-}
-
-output "s3_website_url" {
-  value = aws_s3_bucket.static_site.website_endpoint
-}
--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,9 +1,7 @@
 output "s3_bucket_name" {
-  description = "Nome do bucket S3"
-  value       = aws_s3_bucket.static_site.bucket
+  value = aws_s3_bucket.static_site.bucket
 }
 
 output "s3_website_url" {
-  description = "URL do site estático no S3"
-  value       = aws_s3_bucket.static_site.website_endpoint
+  value = aws_s3_bucket.static_site.website_endpoint
 }
