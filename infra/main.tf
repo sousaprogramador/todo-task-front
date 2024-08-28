c863c901a43dabde0f688fe29bcd2fe7f794333e
Mateus Sousa <sousa.programador@gmail.com>
2024-08-28 13:41:03 -0300

    change bucket polices

--- infra/main.tf
+++ infra/main.tf
@@ -17,7 +17,7 @@ resource "aws_s3_bucket" "static_site" {
 
 resource "aws_s3_bucket_website_configuration" "static_site" {
   count  = data.aws_s3_bucket.existing_bucket.id == "" ? 1 : 0
-  bucket = aws_s3_bucket.static_site.bucket
+  bucket = aws_s3_bucket.static_site[0].bucket
 
   index_document {
     suffix = "index.html"
@@ -30,7 +30,7 @@ resource "aws_s3_bucket_website_configuration" "static_site" {
 
 resource "aws_s3_bucket_public_access_block" "static_site_public_access" {
   count  = data.aws_s3_bucket.existing_bucket.id == "" ? 1 : 0
-  bucket = aws_s3_bucket.static_site.bucket
+  bucket = aws_s3_bucket.static_site[0].bucket
 
   block_public_acls       = false
   ignore_public_acls      = false
@@ -40,7 +40,7 @@ resource "aws_s3_bucket_public_access_block" "static_site_public_access" {
 
 resource "aws_s3_bucket_ownership_controls" "static_site_ownership" {
   count  = data.aws_s3_bucket.existing_bucket.id == "" ? 1 : 0
-  bucket = aws_s3_bucket.static_site.bucket
+  bucket = aws_s3_bucket.static_site[0].bucket
 
   rule {
     object_ownership = "BucketOwnerEnforced"
@@ -49,7 +49,7 @@ resource "aws_s3_bucket_ownership_controls" "static_site_ownership" {
 
 resource "aws_s3_bucket_policy" "static_site_policy" {
   count  = data.aws_s3_bucket.existing_bucket.id == "" ? 1 : 0
-  bucket = aws_s3_bucket.static_site.bucket
+  bucket = aws_s3_bucket.static_site[0].bucket
   policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
@@ -57,7 +57,7 @@ resource "aws_s3_bucket_policy" "static_site_policy" {
         Effect    = "Allow",
         Principal = "*",
         Action    = "s3:GetObject",
-        Resource  = "arn:aws:s3:::${aws_s3_bucket.static_site.bucket}/*"
+        Resource  = "arn:aws:s3:::${aws_s3_bucket.static_site[0].bucket}/*"
       }
     ]
   })
@@ -66,7 +66,7 @@ resource "aws_s3_bucket_policy" "static_site_policy" {
 
 resource "aws_s3_bucket_versioning" "static_site_versioning" {
   count  = data.aws_s3_bucket.existing_bucket.id == "" ? 1 : 0
-  bucket = aws_s3_bucket.static_site.bucket
+  bucket = aws_s3_bucket.static_site[0].bucket
 
   versioning_configuration {
     status = "Enabled"
@@ -74,9 +74,9 @@ resource "aws_s3_bucket_versioning" "static_site_versioning" {
 }
 
 output "s3_bucket_name" {
-  value = data.aws_s3_bucket.existing_bucket.bucket != "" ? data.aws_s3_bucket.existing_bucket.bucket : aws_s3_bucket.static_site.bucket
+  value = data.aws_s3_bucket.existing_bucket.id != "" ? data.aws_s3_bucket.existing_bucket.bucket : aws_s3_bucket.static_site[0].bucket
 }
 
 output "s3_website_url" {
-  value = data.aws_s3_bucket.existing_bucket.website_endpoint != "" ? data.aws_s3_bucket.existing_bucket.website_endpoint : aws_s3_bucket_website_configuration.static_site.website_endpoint
+  value = data.aws_s3_bucket.existing_bucket.id != "" ? data.aws_s3_bucket.existing_bucket.website_endpoint : aws_s3_bucket_website_configuration.static_site[0].website_endpoint
 }
