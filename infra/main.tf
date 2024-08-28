f5be7daddacd77f394f00196111366fc2f5e83d9
Mateus Sousa <sousa.programador@gmail.com>
2024-08-28 13:03:40 -0300

    soluctions end

--- infra/main.tf
+++ infra/main.tf
@@ -2,25 +2,31 @@ provider "aws" {
   region = "sa-east-1"
 }
 
-resource "aws_s3_bucket" "static_site" {
+data "aws_s3_bucket" "existing_bucket" {
   bucket = "todo-site-sousa-dev"
+}
 
-  website {
-    index_document = "index.html"
-    error_document = "error.html"
+resource "aws_s3_bucket" "static_site" {
+  count  = data.aws_s3_bucket.existing_bucket.bucket != "" ? 0 : 1
+  bucket = "todo-site-sousa-dev"
+}
+
+resource "aws_s3_bucket_website_configuration" "static_site_website" {
+  count  = length(aws_s3_bucket.static_site) > 0 ? 1 : 0
+  bucket = aws_s3_bucket.static_site[0].bucket
+
+  index_document {
+    suffix = "index.html"
   }
 
-  versioning {
-    enabled = true
-  }
-
-  lifecycle {
-    ignore_changes = [website, versioning]
+  error_document {
+    key = "error.html"
   }
 }
 
 resource "aws_s3_bucket_policy" "static_site_policy" {
-  bucket = aws_s3_bucket.static_site.bucket
+  count  = length(aws_s3_bucket.static_site) > 0 ? 1 : 0
+  bucket = aws_s3_bucket.static_site[0].bucket
   policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
@@ -28,16 +34,27 @@ resource "aws_s3_bucket_policy" "static_site_policy" {
         Effect    = "Allow",
         Principal = "*",
         Action    = "s3:GetObject",
-        Resource  = "arn:aws:s3:::${aws_s3_bucket.static_site.bucket}/*"
+        Resource  = "arn:aws:s3:::${aws_s3_bucket.static_site[0].bucket}/*"
       }
     ]
   })
 }
 
+resource "aws_s3_bucket_versioning" "static_site_versioning" {
+  count  = length(aws_s3_bucket.static_site) > 0 ? 1 : 0
+  bucket = aws_s3_bucket.static_site[0].bucket
+  versioning_configuration {
+    status = "Enabled"
+  }
+  depends_on = [aws_s3_bucket.static_site]
+}
+
 output "s3_bucket_name" {
-  value = aws_s3_bucket.static_site.bucket
+  value       = data.aws_s3_bucket.existing_bucket.bucket != "" ? data.aws_s3_bucket.existing_bucket.bucket : aws_s3_bucket.static_site[0].bucket
+  description = "O nome do bucket S3"
 }
 
 output "s3_website_url" {
-  value = aws_s3_bucket.static_site.website_endpoint
+  value       = data.aws_s3_bucket.existing_bucket.bucket != "" ? data.aws_s3_bucket.existing_bucket.website_endpoint : aws_s3_bucket_website_configuration.static_site_website[0].website_endpoint
+  description = "A URL do site no S3"
 }
