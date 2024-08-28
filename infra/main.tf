d9cca077f292f54d21b048105f02d8a77950257f
Mateus Sousa <sousa.programador@gmail.com>
2024-08-28 12:28:41 -0300

    output s3 butcket exisits

--- infra/main.tf
+++ infra/main.tf
@@ -4,6 +4,10 @@ provider "aws" {
 
 data "aws_s3_bucket" "existing_bucket" {
   bucket = "todo-site-sousa-dev"
+  # Evita falhas silenciosas se o bucket não existir
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_region_validation      = true
 }
 
 resource "aws_s3_bucket" "static_site" {
@@ -11,13 +15,17 @@ resource "aws_s3_bucket" "static_site" {
   bucket = "todo-site-sousa-dev"
 }
 
-resource "aws_s3_bucket_versioning" "static_site_versioning" {
+resource "aws_s3_bucket_website_configuration" "static_site_website" {
   count  = length(aws_s3_bucket.static_site) > 0 ? 1 : 0
   bucket = aws_s3_bucket.static_site[0].bucket
-  versioning_configuration {
-    status = "Enabled"
+
+  index_document {
+    suffix = "index.html"
   }
-  depends_on = [aws_s3_bucket.static_site]
+
+  error_document {
+    key = "error.html"
+  }
 }
 
 resource "aws_s3_bucket_policy" "static_site_policy" {
@@ -37,22 +45,12 @@ resource "aws_s3_bucket_policy" "static_site_policy" {
   depends_on = [aws_s3_bucket.static_site]
 }
 
-resource "aws_s3_bucket_public_access_block" "public_access_block" {
-  count  = length(aws_s3_bucket.static_site) > 0 ? 1 : 0
-  bucket = aws_s3_bucket.static_site[0].bucket
-
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-}
-
 output "s3_bucket_name" {
-  value       = data.aws_s3_bucket.existing_bucket.bucket != "" ? data.aws_s3_bucket.existing_bucket.bucket : aws_s3_bucket.static_site[0].bucket
+  value       = coalesce(data.aws_s3_bucket.existing_bucket.bucket, aws_s3_bucket.static_site[0].bucket)
   description = "O nome do bucket S3"
 }
 
 output "s3_website_url" {
-  value       = data.aws_s3_bucket.existing_bucket.bucket != "" ? data.aws_s3_bucket.existing_bucket.website_endpoint : aws_s3_bucket.static_site[0].website_endpoint
+  value       = coalesce(data.aws_s3_bucket.existing_bucket.website_endpoint, aws_s3_bucket_website_configuration.static_site_website.website_endpoint)
   description = "A URL do site no S3"
 }
