e91cce690d0e2394f9cf27b3cf206cb70822c6f2
Mateus Sousa <sousa.programador@gmail.com>
2024-08-28 12:52:33 -0300

    soluctions end

--- infra/main.tf
+++ infra/main.tf
@@ -2,31 +2,25 @@ provider "aws" {
   region = "sa-east-1"
 }
 
-data "aws_s3_bucket" "existing_bucket" {
-  bucket = "todo-site-sousa-dev"
-}
-
 resource "aws_s3_bucket" "static_site" {
-  count  = data.aws_s3_bucket.existing_bucket.bucket == "" ? 1 : 0
   bucket = "todo-site-sousa-dev"
-}
 
-resource "aws_s3_bucket_website_configuration" "static_site_website" {
-  count  = length(aws_s3_bucket.static_site) > 0 ? 1 : 0
-  bucket = aws_s3_bucket.static_site[count.index].bucket
-
-  index_document {
-    suffix = "index.html"
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
   }
 
-  error_document {
-    key = "error.html"
+  versioning {
+    enabled = true
+  }
+
+  lifecycle {
+    ignore_changes = [website, versioning]
   }
 }
 
 resource "aws_s3_bucket_policy" "static_site_policy" {
-  count  = length(aws_s3_bucket.static_site) > 0 ? 1 : 0
-  bucket = aws_s3_bucket.static_site[count.index].bucket
+  bucket = aws_s3_bucket.static_site.bucket
   policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
@@ -34,17 +28,16 @@ resource "aws_s3_bucket_policy" "static_site_policy" {
         Effect    = "Allow",
         Principal = "*",
         Action    = "s3:GetObject",
-        Resource  = "arn:aws:s3:::${aws_s3_bucket.static_site[count.index].bucket}/*"
+        Resource  = "arn:aws:s3:::${aws_s3_bucket.static_site.bucket}/*"
       }
     ]
   })
-  depends_on = [aws_s3_bucket.static_site]
 }
 
 output "s3_bucket_name" {
-  value = data.aws_s3_bucket.existing_bucket.bucket != "" ? data.aws_s3_bucket.existing_bucket.bucket : aws_s3_bucket.static_site[0].bucket
+  value = aws_s3_bucket.static_site.bucket
 }
 
 output "s3_website_url" {
-  value = data.aws_s3_bucket.existing_bucket.bucket != "" ? data.aws_s3_bucket.existing_bucket.website_endpoint : aws_s3_bucket_website_configuration.static_site_website[0].website_endpoint
+  value = aws_s3_bucket.static_site.website_endpoint
 }
