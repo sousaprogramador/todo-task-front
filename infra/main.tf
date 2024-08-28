7f28c7a5415c0ea26b09a9afcdb0e84e7ff9c68d
Mateus Sousa <sousa.programador@gmail.com>
2024-08-28 08:55:39 -0300

    change region

--- infra/main.tf
+++ infra/main.tf
@@ -5,14 +5,30 @@ provider "aws" {
 resource "aws_s3_bucket" "static_site" {
   bucket = var.s3_bucket_name
 
-  website {
-    index_document = "index.html"
-    error_document = "error.html"
-  }
-
   versioning {
     enabled = true
   }
+}
+
+resource "aws_s3_bucket_website_configuration" "static_site_website" {
+  bucket = aws_s3_bucket.static_site.bucket
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "public_access_block" {
+  bucket = aws_s3_bucket.static_site.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
 
 resource "aws_s3_bucket_policy" "static_site_policy" {
@@ -31,4 +47,12 @@ resource "aws_s3_bucket_policy" "static_site_policy" {
   ]
 }
 POLICY
+}
+
+output "s3_bucket_name" {
+  value = aws_s3_bucket.static_site.bucket
+}
+
+output "s3_website_url" {
+  value = aws_s3_bucket_website_configuration.static_site_website.website_domain
 }
--- infra/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "s3_bucket_name" {
-  value = aws_s3_bucket.static_site.bucket
-}
-
-output "s3_website_url" {
-  value = aws_s3_bucket.static_site.website_endpoint
-}
